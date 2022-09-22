3fe951b88d97b47601547a0ccad0b7d421197e4d
Eugene Syrotiuk <eugenesyuk@gmail.com>
2022-09-22 14:21:57 +0200

    Removed unused imports

--- src/store/slices/counterSlice.ts
+++ src/store/slices/counterSlice.ts
@@ -1,4 +1,4 @@
-import { AnyAction, createSlice, Dispatch } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 export const counterSlice = createSlice({
   name: "counter",
