211b7dd983caac96db280567a8c564b67cc8624f
Eugene Syrotiuk <eugenesyuk@gmail.com>
2022-09-22 14:21:03 +0200

    Add redux, counter slice

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,20 @@
 import { Layout, Row, Col } from "antd";
 import { Content } from "antd/lib/layout/layout";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementAsync,
+} from "../store/slices/counterSlice";
 import { AppHeader } from "./AppHeader";
 
 export const Home = () => {
+  const counter = useSelector((store: any) => {
+    return store.counter.value;
+  });
+
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <Layout className="layout">
@@ -11,6 +23,14 @@ export const Home = () => {
           <Row justify="center">
             <Col span={16}>
               <h2>Home</h2>
+            </Col>
+            <Col span={16}>
+              <h2>{counter}</h2>
+              <button onClick={() => dispatch(increment())}>+</button>
+              <button onClick={() => dispatch(incrementAsync(5))}>
+                + Async
+              </button>
+              <button onClick={() => dispatch(decrement())}>-</button>
             </Col>
           </Row>
         </Content>
